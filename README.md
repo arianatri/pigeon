18a84e5a42c03fc7bf66f0ab2a3c33b5dbee94f6
theapache64-hotel <theapache64@gmail.com>
2017-02-15 08:33:50 +0530

    .refreshed

--- README.md
+++
@@ -1,8 +0,0 @@
-# pigeon
-A real time android device tracker
-
-### TODOs
-
-- Magnet mode (DONE)
-- Tap marker to zoom (DONE)
-
--- api/src/com/theah64/pigeon/models/Location.java
+++
@@ -1,7 +0,0 @@
-package com.theah64.pigeon.models;
-
-/**
- * Created by theapache64 on 21/1/17.
- */
-public class Location {
-}
